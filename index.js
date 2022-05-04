9621e6716d6f0e44aacf03a7ec23c4044a67b92f
kim <jinweizhong@dragonest.com>
2022-05-04 19:24:23 +0800

    feat:初始化项目

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const Koa = require('koa');
 const app = new Koa();
 const port = process.env.PORT || 8080;
 
-const router = require('./router');
-const routerResponse = require('./middleware/routerResponse')
-const cors = require('./middleware/cors')
 
-app.use(cors());
-app.use(routerResponse())
-app.use(router.routes(), router.allowedMethods());
+const server = require('http').createServer(app.callback())
+const io = require('socket.io')(server)
 
-app.listen(port, () => {
+//监听connect事件
+io.on('connection', socket => {
+    socket.emit('open');//通知客户端已连接
+    console.log('connected');
+
+    //监听disconnect事件
+    socket.on('disconnect', () => {
+        console.log('disconnect')
+    })
+});
+
+server.listen(port, () => {
     console.log(`web静态服务已启动:${port}`);
 });
+
+
+
