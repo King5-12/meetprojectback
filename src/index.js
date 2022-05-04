df4b1306f6f257c4c053b9bb37edef37d224d2ad
kim <jinweizhong@dragonest.com>
2022-05-04 23:57:39 +0800

    feat: 打印输出

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ fs.readFile(path.join(__dirname, './utils/dirty.txt'), 'utf-8', function (err, d
         return
     }
     console.log(data, 'dirtydata')
-    const dirtyList = data.split("\r\n")
+    const dirtyList = data.split("\n")
     console.log(dirtyList, 'dirtyList')
     dirtyMap = makeSensitiveMap(dirtyList)
 });
